0d2249ccadf1ddea137d0ff7b851e9ae687bb5d6
Manuel Quiñones <manuq@laptop.org>
2013-05-08 14:07:40 -0300

    Rename js files

--- js/activity.js
+++ js/activity.js
@@ -1,8 +1,38 @@
-requirejs.config({
-    baseUrl: "lib",
-    paths: {
-        activity: "../js"
-    }
-});
+define(function (require) {
 
-requirejs(["activity/main"]);
+    var activity = require("sugar-html-core/activity");
+    var icons = require("sugar-html-graphics/icons");
+
+    // Initialize the activity.
+
+    activity.setup();
+
+    // Colorize the activity icon.
+
+    var activityIcon = document.getElementById("activity-button")
+        .getElementsByTagName("img")[0];
+
+    var iconInfo = {
+        "uri": activityIcon.src
+    };
+
+    function colorizeActivityIcon(iconInfo) {
+        icons.load(iconInfo, function (data) {
+            activityIcon.src = data;
+        });
+    }
+
+    activity.getXOColor(function (data) {
+        iconInfo.strokeColor = data[0];
+        iconInfo.fillColor = data[1];
+        colorizeActivityIcon(iconInfo);
+    });
+
+    // Make the activity stop with the stop button.
+
+    var stopButton = document.getElementById("stop-button");
+    stopButton.onclick = function () {
+        activity.close();
+    };
+
+});
